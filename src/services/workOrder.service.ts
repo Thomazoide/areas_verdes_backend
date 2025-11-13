a21e43903ba4c0030281704c9dc0755b3e32db26
Thomazoide <ttellerias01@outlook.com>
2025-11-13 17:37:26 -0300

    modulo WorkOrders arreglado

--- src/services/workOrder.service.ts
+++ src/services/workOrder.service.ts
@@ -2,17 +2,28 @@ import { Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { workOrderNotFoundError } from "src/errors/errors";
 import { WorkOrder } from "src/models/workOrder.models";
+import { SuperForm } from "src/models/superForm.model";
 import { Repository } from "typeorm";
 
 @Injectable()
 export class WorkOrderService {
     constructor(
         @InjectRepository(WorkOrder)
-        private readonly repo: Repository<WorkOrder>
+        private readonly repo: Repository<WorkOrder>,
+        @InjectRepository(SuperForm)
+        private readonly sfRepo: Repository<SuperForm>
     ){};
 
 
     async CreateOrUpdateOrder(data: Partial<WorkOrder>): Promise<WorkOrder>{
+        if(data.superFormID) {
+            const sf = await this.sfRepo.findOneBy({
+                id: data.superFormID
+            });
+            if(!sf) throw new Error("super formulario no existente");
+            data.superForm = sf;
+            return await this.repo.save(data)
+        }
         return await this.repo.save(data);
     }
 
